9708e3d7d358e95a68255116dc6594c7247555fa
PavanKS17 <pavansrinivas2@gmail.com>
2024-12-26 02:07:14 -0800

    Completed Precourse2

--- Exercise_3.py
+++ Exercise_3.py
@@ -3,24 +3,38 @@ class Node:
   
     # Function to initialise the node object  
     def __init__(self, data):  
+        self.data = data
+        self.next = None
         
 class LinkedList: 
   
     def __init__(self): 
+        self.head = None
+
+
         
   
     def push(self, new_data): 
-        
-  
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
     # Function to get the middle of  
     # the linked list 
     def printMiddle(self): 
+        slow_ptr = self.head
+        fast_ptr = self.head
+        while fast_ptr and fast_ptr.next:
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+
+        print("The middle element is: ", slow_ptr.data)
 
 # Driver code 
 list1 = LinkedList() 
 list1.push(5) 
 list1.push(4) 
-list1.push(2) 
+list1.push(7) 
 list1.push(3) 
 list1.push(1) 
 list1.printMiddle() 
--- Exercise_5.py
+++ Exercise_5.py
@@ -3,8 +3,59 @@
 # This function is same in both iterative and recursive
 def partition(arr, l, h):
   #write your code here
+  i = (l - 1)  # Index of smaller element
+  pivot = arr[h]  # pivot
+
+  for j in range(l, h):
+
+    # If current element is smaller than or equal to pivot
+    if arr[j] <= pivot:
+        # increment index of smaller element
+        i = i + 1
+        arr[i], arr[j] = arr[j], arr[i]
+
+  arr[i + 1], arr[h] = arr[h], arr[i + 1]
+  return (i + 1)
 
 
 def quickSortIterative(arr, l, h):
   #write your code here
+  size = h - l + 1
+  stack = [0] * (size)
+
+  # initialize top of stack
+  top = -1
+
+  # push initial values of l and h to stack
+  top = top + 1
+  stack[top] = l
+  top = top + 1
+  stack[top] = h
+
+  # Keep popping from stack while is not empty
+  while top >= 0:
+
+    # Pop h and l
+    h = stack[top]
+    top = top - 1
+    l = stack[top]
+    top = top - 1
+
+    # Set pi as partitioning index, arr[p] is now
+    # at right place
+    pi = partition(arr, l, h)
+
+    # If there are elements on left side of pi
+    if pi - 1 > l:
+        top = top + 1
+        stack[top] = l
+        top = top + 1
+        stack[top] = pi - 1
+
+    # If there are elements on right side of pi
+    if pi + 1 < h:
+        top = top + 1
+        stack[top] = pi + 1
+        top = top + 1
+        stack[top] = h
 
